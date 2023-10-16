0e39d59a6c158522c4101e3b7124e914409cc24d
Antonio <nonypot123@gmail.com>
2023-10-15 22:35:51 -0300

    current day, almost finishing the login screen

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import TelaLogin from './TelaLogin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TelaLogin />
   </React.StrictMode>
 );
