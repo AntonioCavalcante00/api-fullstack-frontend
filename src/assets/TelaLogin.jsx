455c6d354dc70a95f52623cd368a60a6375daa69
Antonio <nonypot123@gmail.com>
2023-10-20 06:11:04 -0300

    #

--- src/assets/TelaLogin.jsx
+++ src/assets/TelaLogin.jsx
@@ -1,18 +1,21 @@
 import './TelaLogin.css';
 
+
 import { AiOutlineUser } from 'react-icons/ai';
 
 import Icone_Logo from '../img/icone_logo.png';
 import PasswordHide from './PasswordHide';
 
-function App() {
+function TelaLogin() {
+
+
   return (
-    <div className='App'>
+    <>
       <form>
         <div className='Container'>
           <div className='Icon-Logo'>
             <img src={Icone_Logo} alt='logo da escola E.E.E.P Adriano Nobre' />
-          </div>  
+          </div>
 
           <div className='Title'>
             <h1>Biblioteca</h1>
@@ -20,29 +23,40 @@ function App() {
 
           <div className='Campo-Group'>
             <AiOutlineUser className='Icon' />
-            <input type='text' name='name' placeholder='Nome' required />
+            <input type='text'
+              name='name'
+              placeholder='Nome'
+              required
+            />
           </div>
 
           <PasswordHide />
 
-          <div className='Remember'>
-            <input type="checkbox" name="checkbox"/>
-            <span>Lembrar-me</span>
-          </div> 
+          <div className='Container2'>
+            <div className='Remember'>
+              <input
+                type="checkbox"
+                name="checkbox"
+                className='Checkbox'
+              />
+              <span>Lembrar-me</span>
+            </div>
 
-          <div className='Links'>
-            <a href='/#' className='Help-A'>Ajuda</a>
+            <div className='Links'>
+              <a href='/#' className='Help-A'>Ajuda</a>
+            </div>
           </div>
-
           <div className='Campo-Group'>
-            <input type='submit'
+            <input
+              type='submit'
               className='Button'
-              value='Entrar' />
+              value='Entrar'
+            />
           </div>
         </div>
       </form>
-    </div>
+    </>
   );
 }
 
-export default App;
+export default TelaLogin;
